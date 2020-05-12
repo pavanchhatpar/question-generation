c37f05e46e1ca43dc2b0fe2b7019834a2eca29e3
pavanchhatpar <chhatpar.p@husky.neu.edu>
2020-05-11 22:22:45 -0400

    hyperparameter changes

--- predict.py
+++ predict.py
@@ -12,9 +12,13 @@ from text_gan.models import CANPZ_Q
 from text_gan.data.squad1_ca_qc import SQuAD_CA_QC
 from text_gan.models import CANP_QC
 
+from text_gan.data.squad_ca_preqc import SQuAD_CA_PreQC
+from text_gan.models import CANP_PreQC
+
 MODELS = [
     "canpz-q",
     "canp-qc",
+    "canp-preqc",
 ]
 
 
@@ -175,6 +179,82 @@ def canp_qc():
         i += 1
 
 
+def canp_preqc():
+    RNG_SEED = 11
+    data = SQuAD_CA_PreQC()
+    to_gpu = tf.data.experimental.copy_to_device("/gpu:0")
+    data = data.train.shuffle(
+        buffer_size=10000, seed=RNG_SEED, reshuffle_each_iteration=False)
+    train = data.take(10).batch(128).apply(to_gpu)
+    val = data.skip(cfg.TRAIN_SIZE).take(10).batch(128).apply(to_gpu)
+    with tf.device("/gpu:0"):
+        train = train.prefetch(1)
+        val = val.prefetch(1)
+    if cfg.EMBS_TYPE == 'glove':
+        embedding_reader = GloVeReader()
+    elif cfg.EMBS_TYPE == 'fasttext':
+        embedding_reader = FastTextReader()
+    else:
+        raise ValueError(f"Unsupported embeddings type {cfg.EMBS_TYPE}")
+    vocab = Vocab.load(
+        embedding_reader.START,
+        embedding_reader.END,
+        embedding_reader.PAD,
+        embedding_reader.UNK,
+        cfg.CSEQ_LEN,
+        cfg.QSEQ_LEN,
+        cfg.VOCAB_SAVE
+    )
+    ner = NERTagger(cfg.NER_TAGS_FILE, cfg.CSEQ_LEN)
+    pos = PosTagger(cfg.POS_TAGS_FILE, cfg.CSEQ_LEN)
+
+    model = CANP_PreQC(vocab, ner, pos)
+    model.load(cfg.MODEL_SAVE)
+    pred, logprobas = model.predict(val)
+    i = 0
+    for X, y in val.unbatch():
+        cis, cit, answer, ner, pos, preq = X
+        qit, qis = y
+        context = vocab.inverse_transform([cis.numpy()], "source")[0]
+        ogques = vocab.inverse_transform([qit.numpy()], "target")[0]
+        ogpref = vocab.inverse_transform([preq.numpy()], "target")[0]
+        ans = ''
+        for j, ai in enumerate(answer):
+            if ai == 0:
+                continue
+            ans += vocab.get_token_text(cis[j].numpy(), "source") + ' '
+        context = filter(
+            lambda w: w != embedding_reader.PAD, context)
+        # try:
+        #     ogques = ogques[:np.where(ogques == embedding_reader.END)[0][0]]
+        # except:  # noqa
+        #     pass
+        # ques = vocab.inverse_transform(pred[i].numpy(), "target")
+        # try:
+        #     ques = ques[:np.where(ques == embedding_reader.END)[0][0]]
+        # except:  # noqa
+        #     pass
+        # attn_weight, idxs = tf.math.top_k(attn_weights[i][1:6], k=3)
+        # attn_tokens = tf.gather(X[0], idxs, axis=-1, batch_dims=0)[0]
+        # attn_tokens = vocab.inverse_transform(attn_tokens.numpy(), "source")
+        print(f"Context:- {' '.join(context)}")
+        print(f"Answer:- {ans}")
+        print(f"Ques prefix:- {' '.join(ogpref)}")
+        print(f"OG Suffix:- {' '.join(ogques)}")
+
+        print(f"Top Suffixes:")
+        for j in range(10):
+            p = idx2str(pred[i][j].numpy(), cis.numpy(), vocab)
+            print(f"Predicted: {' '.join(p)}\t"
+                  f"Proba: {tf.exp(logprobas[i][j])}")
+        # print(f"Log probs:- {logprobas[i]}")
+        # print(
+        #     f"Top attentive words for first 5 question tokens\n {attn_tokens}")
+        # print(f"Attention Weights:- {attn_weight}")
+        print("")
+        i += 1
+
+
 def idx2str(pred_y, X, vocab):
     ret = []
     vocab_len = vocab.get_vocab_size("target")
@@ -189,6 +269,7 @@ def idx2str(pred_y, X, vocab):
 MODEL_METHODS = {
     "canpz-q": canpz_q,
     "canp-qc": canp_qc,
+    "canp-preqc": canp_preqc,
 }
 
 
